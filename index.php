ce7654f23cc406975028866d02fa8c73ba34fab9
user <lo@lo.lo>
2021-09-16 11:58:31 +0300

    basic event loop with

--- index.php
+++ index.php
@@ -1,21 +1,40 @@
 <?php
 echo var_dump(use_promise());
+require_once "Promise.php";
+require_once "PromiseStatus.php";
 
-setTimeout(function(){
-  sleep(1);
-echo "bye";
-foreach([1,2,3,4,5] as $key=>$value){
-var_dump($key, $value);
-}
-}, 2000);
+// $promise = new Promise(function($resolve, $reject){
+// setTimeout(function()use ($resolve){
+//   sleep(1);
+// echo "bye";
+// foreach([1,2,3,4,5] as $key=>$value){
+// var_dump($key, $value);
+// }
+// $resolve("promise finshed");
+// }, 0);
+// });
+$promise = Promise::reject("error");
+$promise->catch(
+fn(...$args)=>var_dump($args)
+);
+$promise->then(
+fn(...$args)=>var_dump($args)
+);
+$promise->finally(
+fn()=>var_dump("args")
+);
+$promise->finally(
+fn()=>var_dump("args")
+);
 
-setTimeout(function(){
-  sleep(1);
-echo "bye";
-foreach(["mama", "papa","syn"] as $key=>$value){
-var_dump($key, $value);
-}
-}, 1000);
+
+// setTimeout(function(){
+//   sleep(1);
+// echo "bye";
+// foreach(["mama", "papa","syn"] as $key=>$value){
+// var_dump($key, $value);
+// }
+// }, 1000);
 echo "hello6786486787695686784875979\n";
 
   sleep(1);
